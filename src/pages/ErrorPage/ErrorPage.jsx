763d7b911bf3e1b8d1edf3a3dc29b2c8fe35a89e
rockethelll <51137055+rockethelll@users.noreply.github.com>
2024-02-28 15:13:05 +0100

    fix lint errors

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
 
 const ErrorPage = () => {
   return (
